9fb25ba73cfd4fa4f66acaaaf6ee92952c45ba0a
MARRIPATI GIRISH <girish.marripati@gmail.com>
2019-12-03 16:59:17 +0530

    program2

---
+++ Comparator.java
@@ -0,0 +1,115 @@
+package java;
+
+public class Comparator {
+
+}
+public static void main(String[] args)
+public class Employee implements comparable<Employee>{
+	int empNO;
+	String empName;
+	public Employee(int emp No,string empName) {
+		super ();
+		this.empNO=empNo;
+		this.empName=EmpName;
+		
+	}
+	override
+	public int compare To(Employee 0) {
+		if 9this.empNO>o.empNO)
+return 1;
+else
+	return-1;
+	}
+	import java.util.ArryList;
+	import java.util.collections;
+	import java.util.instructors
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	}
+}
